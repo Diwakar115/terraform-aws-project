9f191690447283a45402df8d1b0992a4768624f9
Diwakar Naidu <diwakarnaidu8@gmail.com>
2025-09-02 17:17:26 +0530

    terraform+aws project

---
+++ main.tf
@@ -0,0 +1,13 @@
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-02d26659fd82cf299" # Amazon Linux 2 (us-east-1)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = "myterraformawsbucket"
+}
+
