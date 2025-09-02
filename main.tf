b7cde01e4d245ede02a36d157fddbbd8c8e75e94
Diwakar Naidu <diwakarnaidu8@gmail.com>
2025-09-02 19:02:06 +0530

    new changes

--- main.tf
+++ main.tf
@@ -11,3 +11,4 @@ resource "aws_instance" "my_ec2" {
 # S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
+}
--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,3 @@ output "ec2_public_ip" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
-`
