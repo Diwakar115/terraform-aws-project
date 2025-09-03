a99db46178feb4c6e7ce5e1ad35a684f15c3bfa9
Diwakar115 <diwakarnaidu8@gmail.com>
2025-09-03 13:09:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket         = "myterraformawsbucket1230"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
   }
 }
