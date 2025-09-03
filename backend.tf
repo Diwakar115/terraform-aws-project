ba93365be5f5bf7a2b271786dbd17862d1f4a2c5
Diwakar115 <diwakarnaidu8@gmail.com>
2025-09-03 13:13:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "myterraformawsbucket1230"
+    bucket         = "myterraformawsbucket052000"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
   }
